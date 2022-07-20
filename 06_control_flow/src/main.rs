d000956da6b66558a407a5083ca2b23cc78189ea
Sayakie <sayakie@kakao.com>
2022-07-20 02:24:48 +0000

    feat(c06): handle multiple conditions with `else if`

--- 06_control_flow/src/main.rs
+++ 06_control_flow/src/main.rs
@@ -11,4 +11,14 @@ fn main() {
     if number != 0 {
         println!("number was something other than zero");
     }
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 }
